2ad4b42d8235250642713ee15f5e153abc5a638e
BMarsili <62031093+BeatriceMarsili@users.noreply.github.com>
2022-06-07 09:02:24 +0200

    Dir handling
    Adjusted directory handling, added logging and possibility to add next_cursor in the script's call

--- retrieve_followers0.py
+++ retrieve_followers0.py
@@ -11,6 +11,7 @@ import json
 import time
 import argparse
 import datetime
+import logging
 
 
 def create_url(user_id, token, type=None):
@@ -69,7 +70,7 @@ def retrieve_small(user_id, count, followers):
             retrieved = len(response_lst)
             missing = followers - retrieved 
             print(str(datetime.datetime.now()),'Sleeping, I retrieved information about {} accounts. I still need to retrieve information about {} accounts'.format(retrieved, missing))
-            time.sleep(905)             #sleeps for 15 mins and 5 seconds to respect the Twitter limitation of 15 requests x 15 minutes https://developer.twitter.com/en/docs/twitter-api/users/follows/api-reference/get-users-id-followers
+            time.sleep(905)             #sleeps for 15 mins and 5 seconds to respect the Tweeter limitation of 15 requests x 15 minutes https://developer.twitter.com/en/docs/twitter-api/users/follows/api-reference/get-users-id-followers
         url = create_url(user_id, next_token)
         json_response = connect_to_endpoint(url)
         count+=1
@@ -86,23 +87,26 @@ def retrieve_small(user_id, count, followers):
 
 
 
-def retrieve_big(user_id, count, followers):
+def retrieve_big(next_token, user_id, count, followers):
     '''
     Gets as input an user id and saves to a JSON file (in the data directory) data regading all the followers of the user. Retrieves 15k followers and then sleeps 15 minutes. 
     '''
-    next_token= None
     if count == None:
         count = 0
     response_lst= []
+    logger = logging.getLogger()
+    handler = logging.FileHandler("data/logs/logfile{}.log".format(datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S.%f")))
+    logger.addHandler(handler)
     while count*5000<followers:  
         if count%15==0 and count!=0:
             retrieved = count*5000
             missing = followers - retrieved
             with open("data/response{}.json".format(user_id), "a") as f:
                 f.write(json.dumps(response_lst))
-                next_token = None
-                response_lst=[] 
-            print(str(datetime.datetime.now()),'Sleeping, I retrieved the ids for {} accounts. \nI still need to retrieve the ids for {} accounts'.format(retrieved, missing))
+                response_lst=[]
+            message= str(datetime.datetime.now()),'Sleeping, I retrieved the ids for {} accounts. I still need to retrieve the ids for {} accounts. Next token: {}. User_id being handled:{}'.format(retrieved, missing, next_token, user_id)     
+            logger.error(message)
+            print(str(datetime.datetime.now()),'Sleeping, I retrieved the ids for {} accounts. \nI still need to retrieve the ids for {} accounts. Next token: {}'.format(retrieved, missing, next_token))
             time.sleep(905)             #sleeps for 15 mins and 5 seconds to respect the Twitter limitation of 15 requests x 15 minutes https://developer.twitter.com/en/docs/twitter-api/users/follows/api-reference/get-users-id-followers
         url = create_url(user_id, next_token, 'big')
         json_response = connect_to_endpoint(url)
@@ -124,19 +128,26 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='Twitter-followers',
     description='''This scripts retrieves information about the Twitter followers of the account(s) provided as id in the args.
                         \nThe IDs must be provided (-l --list) as a string with comma-separated values i.e. "2244994945, 1334200897081987072".
-                        \nBy deafault, if the account(s) provided have less than 75K of followers, several information about the followers ( as number of posted tweets, username, location (if available) and number of followers) will be retrieved, 
-                        but this process is quite long, it will retrieve information about 15K followers and then stop for 15minutes.
+                        \nBy deafault, if the account(s) provided have less than 75K of followers, several information about the followers will be retrieved, 
+                        as number of posted tweets, username, location (if available) and number of followers but this process is quite long, it will retrieve 15K followers each 15minutes.
                         \nFor accounts that have more than 75K followers just the ids of the followers account will be retrieved, this process obtains 75K of IDs each 15minutes. 
                         \nThis treshold of 75K can be changed with the optional parameter -t --treshold
+                        \nIt is also possible to insert a cursor (-c --cursor) (given by Twitter in previous requests) so that if one has to stop the script the search can be retrived from where it stopped.
                         ''')
 
     parser.add_argument('-l', '--list', help='List of IDs', type=str, required=True)
     parser.add_argument('-t', '--treshold', help='Treshold to define if an account is big or small. Default value = 75K', type=int,required=False)
-
+    parser.add_argument('-c', '--cursor', help='Cursor to start the search from a certain point. Given by Twitter in the previous request. Default=None', type=int, required=False)
     args = parser.parse_args()
-
     ids = [int(item)for item in args.list.split(',')]
-    treshold = args.treshold    
+    treshold = args.treshold 
+    token = args.cursor
+    data_dir ='\data'
+    log_dir = '\data\logs'
+    if not os.path.exists(os.getcwd()+data_dir):
+        os.makedirs(os.getcwd()+data_dir)
+    if not os.path.exists(os.getcwd()+log_dir):
+        os.makedirs(os.getcwd()+log_dir)
     if treshold==None:
         treshold=75000
     count_small = None
@@ -148,4 +159,5 @@ if __name__ == '__main__':
             count_small = retrieve_small(user_id, count_small, followers)
         else: 
             print('I will retrieve the IDs of the followers of account {}'.format(user_id))
-            count_big = retrieve_big(user_id, count_big, followers)
+            count_big = retrieve_big(token, user_id, count_big, followers)
+
