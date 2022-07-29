61fea1dad9583559d9fe532df93a8828dae236ba
BMarsili <62031093+BeatriceMarsili@users.noreply.github.com>
2022-07-29 14:14:54 +0200

    update prints

--- retrieve_following.py
+++ retrieve_following.py
@@ -32,7 +32,7 @@ def bearer_oauth(r):
     if bearer_token==None:
         raise ValueError("Please set the Bearer Token of your Twitter Application as env variable")
     r.headers["Authorization"] = f"Bearer {bearer_token}"
-    r.headers["User-Agent"] = "UnMappers Analysis"
+    r.headers["User-Agent"] = "Twitter Analysis"
     return r
 
 def connect_to_endpoint(url, count):
@@ -60,9 +60,9 @@ def retrieve_ids():
             press('enter')
             if count % 15 ==0 and count !=0: 
                 time_left = est_time(missing)
-                message= print_time()+'Sleeping, I retrieved the ids for {} accounts. I still need to retrieve the ids for {} accounts.'.format(count, missing)     
+                message= print_time()+'Sleeping, I retrieved the information for {} accounts. I still need to retrieve the information for {} accounts.'.format(count, missing)     
                 logger.error(message)
-                print(print_time(),'Sleeping, I retrieved the ids for {} accounts. \nI still need to retrieve the ids for {} accounts. \nEstimated time left: {}'.format(count, missing, time_left))
+                print(print_time(),'Sleeping, I retrieved the information for {} accounts. \nI still need to retrieve the information for {} accounts. \nEstimated time left: {}'.format(count, missing, time_left))
                 time.sleep(905) 
                 press('enter')
             user_id = line.split('/')[5]
@@ -72,7 +72,7 @@ def retrieve_ids():
             with open("data/following_grade0/response{}.json".format(user_id), "w") as f:
                 f.write(json.dumps(json_response))
                 f.close()
-                print("Ids of the account that user {} follows saved at \"data/response{}.json\"".format(user_id, user_id))
+                print("Information about the accounts that user {} follows saved at \"data/following_grade0/response{}.json\"".format(user_id, user_id))
                 
     return 
 
@@ -80,7 +80,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='Twitter-followers',
     description='''This scripts retrieves the ids of the accounts followed by the accounts in input.
                         \nThe IDs must be provided in a .txt file containing the requests to the Twitter api, assumes the accounts follow less than 800 other accounts, therefore doens't need the parameter max_results.
-                        \nExpects a file in data/database_ids/target_requests.json containing the list of the requests to be made. Format: 'https://api.twitter.com/2/users/$user_id$/following?user.fields=$desired user fileds$\'''')
+                        \nExpects a file in data/database_ids/target_requests.json containing the list of the requests to be made. Format of the requests: 'https://api.twitter.com/2/users/$user_id$/following?user.fields=$desired user fileds$\'''')
     log_dir = '/data/following_grade0/logs'
     if not os.path.exists(os.getcwd()+log_dir):
         os.makedirs(os.getcwd()+log_dir)
